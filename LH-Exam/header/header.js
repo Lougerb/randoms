600a0183cdd80d625b21e8923117e448619ed512
LougerB <glouien@gmail.com>
2021-08-04 20:51:30 +0800

    LH-header finished

---
+++ LH-Exam/header/header.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById("nav-hamburger");
+const nav = document.getElementById("header-2");
+const close = document.getElementById("close");
+const sandwich = document.getElementById("sandwich");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("nav-show");
+  sandwich.style.display = sandwich.style.display == "none" ? "flex" : "none";
+  close.style.display = close.style.display == "flex" ? "none" : "flex";
+});
