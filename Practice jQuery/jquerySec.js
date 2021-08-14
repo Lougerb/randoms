6b5eaec7be6fdaff02a6b3999acb3d6586ea2693
LougerB <glouien@gmail.com>
2021-08-14 15:39:54 +0800

    added section

--- Practice jQuery/jquerySec.js
+++ Practice jQuery/jquerySec.js
@@ -1,1 +1,14 @@
+// Syntax
+// $("insertElement ID or Selector here").jQueryFunction();
+
+// Example
+
+// To hide an element
 $(".p-1").hide();
+
+// To change style
+// of the list
+$(".container ul li").css("color", "green");
+$(".container ul li").css("font-weight", "700");
+$(".container ul li").css("background-color", "#aaa");
+$(".container ul li:even").css("background-color", "#eee");
